8f3fbcdd8573785458c27821d49292048c4e7b7f
ImBlindForU <samuele.zanchi10@gmail.com>
2022-12-15 16:03:06 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="{{ route("home")}}">home</a></li>
+                {{-- <li><a href="{{ route("characters")}}">characters</a></li>
+                <li><a href="{{ route("comics")}}">comics</a></li>
+                <li><a href="{{ route("movies")}}">home</a></li>
+                <li><a href="{{ route("tv")}}">tv</a></li>
+                <li><a href="{{ route("games")}}">games</a></li>
+                <li><a href="{{ route("collectibiels")}}">collectibiels</a></li>
+                <li><a href="{{ route("videos")}}">videos</a></li>
+                <li><a href="{{ route("fans")}}">fans</a></li>
+                <li><a href="{{ route("news")}}">news</a></li>
+                <li><a href="{{ route("shop")}}">shop</a></li> --}}
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})-> name('home');
+
+Route::get('/charachters', function () {
+    return view('charachters');
+})-> name('charachters');
+
+Route::get('/comics', function () {
+    return view('comics');
+})-> name('comics');
+
+// Route::get('/movies', function () {
+//     return view('movies');
+// })-> name('movies');
+
+// Route::get('/tv', function () {
+//     return view('tv');
+// })-> name('tv');
+
+// Route::get('/games', function () {
+//     return view('games');
+// })-> name('games');
+
+// Route::get('/collectibles', function () {
+//     return view('collectibles');
+// })-> name('collectibles');
+// Route::get('/video', function () {
+//     return view('video');
+// })-> name('video');
+
+// Route::get('/fans', function () {
+//     return view('fans');
+// })-> name('fans');
+
+// Route::get('/news', function () {
+//     return view('news');
+// })-> name('news');
+
+// Route::get('/shop', function () {
+//     return view('shop');
+// })-> name('shop');
